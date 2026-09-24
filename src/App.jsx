7f4274ab17fdae76ffe31321a96f44b8f215b7a5
Kooji <guikooji11@hotmail.com>
2026-09-24 11:22:00 -0300

    feat: Codificando e estilizando o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Contato from'./pages/login'
+import Error from './pages/Error'
+import Jogos from './pages/Jogos'
+import Login from './pages/Login'
 
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="min-h-screen flex flex-col justify-between bg-[#141414] pt-4">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Jogos/>}/>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Error/>}/>
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
   )
 }
 
